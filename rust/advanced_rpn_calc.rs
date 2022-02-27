7cc32f3504de46c52e6b1cc46377dfe7f6e33dd8
Mikutut <marcinmikula840@gmail.com>
2022-02-27 12:37:08 +0100

    UPDATE: Added factorial and power operations to advanced_rpn_calc

--- rust/advanced_rpn_calc.rs
+++ rust/advanced_rpn_calc.rs
@@ -16,6 +16,7 @@ fn main() {
 
       if let Ok(i) = op.parse::<i64>() {
         stack.push(i);
+				//println!("Stack count: {}", stack.len().to_string());
       } else {
         match op {
           "+" => {
@@ -74,17 +75,17 @@ fn main() {
               panic!("Incorrect operands count on stack!");
             } else {
               let mut result: i64 = 0;
-              for i in 0..stack.len() {
-                let opd = stack.remove(0);
-                if i == 0 {
-                  result = opd;
-                } else {
-                  if opd != 0 {
-                    result /= opd;
-                  } else {
-                    panic!("Division by zero is forbidden!");
-                  }
-                }
+              for i in 0..stack.len() { 
+								let opd = stack.remove(0);
+								if i == 0 {
+									result = opd;
+								} else {
+									if opd != 0 {
+										result /= opd;
+									} else {
+										panic!("Division by zero is forbidden!");
+									}
+								}
               }
 
               stack.push(result);
@@ -111,12 +112,44 @@ fn main() {
               stack.push(result);
             }
           }
+					"!" => {
+						if stack.len() < 1 {
+							panic!("Incorrect operands count on stack!");
+						} else {
+							let mut mul = stack.remove(0);
+							let mut result: i64 = mul;
+							mul -= 1;
+							for i in (1..(mul+1)).rev() {
+								result *= i;
+							}
+							stack.push(result);
+						}
+					}
+					"^" => {
+						if stack.len() != 2 {
+							panic!("Incorrect operands count on stack!");
+						} else {
+							let opd_a = stack.remove(0);
+							let opd_b = stack.remove(0);
+							let mut result: i64 = 0;
+
+							for i in 0..opd_b {
+								if i == 0 {
+									result = opd_a;
+								} else {
+									result *= opd_a;
+								}
+							}
+
+							stack.push(result);
+						}
+					}
           _ => {
             panic!("Correct operator nor operand provided!");
           }
-        }
-      }
-    }
+				}
+    	}
+		}
     println!("{}", stack.pop().unwrap());
   }
 }
